a9d0a9e117b89c1c8c2e57288b42386cea611512
fahimSiam <siamfahim02@gmail.com>
2023-05-15 01:28:04 +0600

    Update server.js

--- server.js
+++ server.js
@@ -69,3 +69,4 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+//I followed a youtube tutorial to do this. link: https://www.youtube.com/watch?v=jD7FnbI76Hg
